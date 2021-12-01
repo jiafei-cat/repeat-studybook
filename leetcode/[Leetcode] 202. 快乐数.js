64ca84ff68f1ce94387d6bbf2874ff26ff2bab19
qetuozm <yinjianihao@qq.com>
2021-12-02 00:03:13 +0800

    feat: leetcode 202 add fast slow pointer

--- leetcode/[Leetcode] 202. 快乐数.js
+++ leetcode/[Leetcode] 202. 快乐数.js
@@ -8,6 +8,8 @@ const getNext = n => {
   return total
 }
 
+const getNext = n => String(n).split('').reduce((pre, cur) => pre + Math.pow(cur, 2), 0)
+
 const isHappy = n => {
   const visited = new Set()
   while (n !== 1 && getNext(n) !== 1) {
@@ -17,6 +19,26 @@ const isHappy = n => {
 
     visited.add(n)
     n = getNext(n)
+  }
+
+  return true
+}
+
+const isHappy = n => {
+  let fast = n
+  let slow = n
+
+  while(fast !== 1 && getNext(fast) !== 1) {
+    fast = getNext(getNext(fast))
+    slow = getNext(slow)
+
+    if (fast === 1) {
+      return true
+    }
+
+    if (fast === slow) {
+      return false
+    }
   }
 
   return true
