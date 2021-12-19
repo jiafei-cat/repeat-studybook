4cf993adc7f0fb7ed41b1eea151760ad313176f5
qetuozm <yinjianihao@qq.com>
2021-12-19 21:59:58 +0800

    doc: update md and update comments

--- leetcode/[Leetcode] 206.反转链表.js
+++ leetcode/[Leetcode] 206.反转链表.js
@@ -1,76 +1,29 @@
-// 迭代做法
-const reverserList = head => {
-  let cur = head
-  let pre = null
-
-  while (cur) {
-    let temp = cur.next
-    cur.next = pre
-    pre = cur
-    cur = temp
-  }
-
-  return pre
-}
-
-
-// 递归做法 - 1, 每次都将当前next指针赋值为null
-const reverserList = head => {
+// 递归法
+function reverseList(head) {
+  // 递归终止条件
+  // 因为我们需要将当前节点的下个节点的next指针指向自己所以要保证head.next指针的存在
   if (!head || !head.next) {
-    return head
+      return head // 返回往下最后一个节点，最后一个节点既是反转后的第一个节点
   }
-
-  const result = reverserList(head.next)
+  
+  const result = reverseList(head.next)
   head.next.next = head
   head.next = null
-
+  
   return result
 }
 
-// 递归做法 - 2, 借助变量将记录递归次数，当0时才将头节点next赋值为null
-const reverserList = (head, time = 0) => {
-  if (!head || !head.next) {
-    return head
+// 迭代法
+const reverseList = head => {
+  let cur = head
+  let pre = null
+  while (cur) {
+      let temp = cur.next // 因为下面要改变当前next指针，存储next指针以便下一次遍历
+      cur.next = pre
+      
+      pre = cur // 指向上次节点, 便于下次遍历使用
+      cur = temp // 遍历下一个节点
   }
-
-  const result = reverseList(head.next, time + 1)
-  head.next.next = head
-  if (time === 0) {
-    head.next = null
-  }
-
-  return result
-}
-
-
-// 递归n个翻转
-const reverseList = (head, time = 0, number = 3) => {
-  if (!head || !head.next) {
-    return [head, head.next]
-  }
-
-  const [result, lastNodeNext] = reverserList(head.next, time + 1, number - 1)
-  head.next.next = head
-
-  if (time === 0) {
-    head.next = lastNodeNext
-  }
-
-  return time === ? result : [result, lastNodeNext]
-}
-
-
-// 简化递归n个翻转 - 借助每次的next指针
-const reverseList = (head, number = 3) => {
-  if (!head || !head.next) {
-    return head
-  }  
-
-  let temp = head.next
-  const result = reverseList(head.next, number - 1)
-  head.next = temp.next
-  temp.next = head
-
-
-  return result
+  
+  return pre // 反转后最后一个节点既是头节点
 }
--- leetcode/[Leetcode] 92.反转链表2.js
+++ leetcode/[Leetcode] 92.反转链表2.js
@@ -1,27 +1,33 @@
-const reverse = (head, n) => {
-  if (n === 1) {
+var reverseBetween = function(head, left, right) {
+  // 通过示例和提示范围得知翻转次数为right减left加1(提示left和right都小于链表长度且大于1)
+  const time = right - left + 1
+  // 头节点可以会被翻转，创建个虚拟头节点方便操作头节点
+  const dummy = new ListNode(null, head)
+  let p = dummy
+
+  // 找到翻转前的头一个节点，为了方便翻转后拼接(所以这里是--left)
+  while (--left) {
+    p = p.next
+  }
+
+  // 拼接翻转后的节点
+  p.next = reverse(p.next, time)
+
+  return dummy.next // 返回虚拟
+};
+
+function reverse(head, n) {
+  // 翻转次数为1了就不需要翻转了，返回翻转后的第一个节点
+  if (!head || !head.next || n === 1) {
     return head
   }
 
-  const temp = head.next
   const result = reverse(head.next, n - 1)
 
-  head.next = temp.next
+  // 递归往上的过程要修改head的next指针，所以在往下的过程保存head的next指针便于操作
+  let temp = head.next
+  head.next = head.next.next // 传递最后一个节点的next指针
   temp.next = head
 
   return result
-}
-
-
-const reverseBetween = (head, left, right) => {
-  const cha = right - left + 1
-  const newListNode = new ListNode(null, head)
-  let cur = newListNode
-  while (--left) {
-    cur = cur.next
-  }
-
-  cur.next = reverse(cur.next, cha)
-
-  return newListNode.next
 }
