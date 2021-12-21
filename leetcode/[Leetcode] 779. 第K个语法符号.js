8d4bdd8611077b50f64186fc6eff6db3b6756e98
qetuozm <yinjianihao@qq.com>
2021-12-21 23:53:24 +0800

    feat: add 779 update comments

--- leetcode/[Leetcode] 779. 第K个语法符号.js
+++ leetcode/[Leetcode] 779. 第K个语法符号.js
@@ -6,10 +6,13 @@ var kthGrammar = function(n, k) {
   const n1 = Math.pow(2, n - 1) // 计算第n行长度
   const isLeft = k <= n1 / 2
 
+  // 每次递归都会判断是否在左边
   if (isLeft) {
     return kthGrammar(n - 1, k)
   } else {
+    // 如果在右边，那么下次查找的位置减少一半
     let val = kthGrammar(n - 1, k - n1 / 2)
+    // 结果需要翻转，因为是镜像关系
     return val === 0 ? 1 : 0
   }
 
