de0693f514b71b8e8a6bb5dc3ba31ea3a0e3c58f
qetuozm <yinjianihao@qq.com>
2022-02-15 22:27:14 +0800

    feat: add inner type utils

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,21 +5,3 @@
 // import './typeGuard' // 类型守卫
 import './typeAsserts' // 类型断言
 // import './tips' // 小技巧
-
-
-interface Array2 {
-  0: number
-  1: string
-}
-
-const a: Array2 = [1,'2',3]
-const b: Array<number> = [1,2,3,4]
-
-const list = [1, 2, 3] as const // TS3.4 syntax
-type NeededUnionType = typeof list[number]
-
-type List2 = [1, 2, 3]
-type List3 = keyof List2
-type List4 = {
-  [P in List2[number]]: P
-}
--- typescript/src/typeGymnastics/index.ts
+++ typescript/src/typeGymnastics/index.ts
@@ -1,5 +1,23 @@
-// Pick
 // ==================================
+// interface
+// ==================================
+
+// Partial
+type MyPartial<T> = {
+  [P in keyof T]?: T[P]
+}
+
+// Required
+type MyRequired<T> = {
+  [P in keyof T]-?: T[P]
+}
+
+// Readonly
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+// Pick
 type MyPick<T, K extends keyof T> = {
   [P in K]: T[P]
 }
@@ -17,10 +35,6 @@ const todo: TodoPreview = {
   completed: false,
 }
 
-// Exclude
-// ==================================
-type MyExclude<T, K> = T extends K ? never : T
-type Test1 = MyExclude<"a" | "b" | "c", "a">
 
 // Omit
 interface Todo {
@@ -42,6 +56,51 @@ type MyOmit2<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
 type Test2 = Omit<Todo, 'description'>
 
+// ==================================
+// Union type
+// ==================================
+
+// Exclude
+type MyExclude<T, K> = T extends K ? never : T
+type MyExcludeTest = MyExclude<"a" | "b" | "c", "a"> // 'b' | 'c'
+
+// Extract
+type MyExtract<T, K> = T extends K ? T : never
+type MyExtractTest = MyExtract<"a" | "b" | "c", "a"> // 'a'
+
+// NonNullable
+type MyNonNullable<T> = T extends null | undefined ? never : T;
+type MyNonNullableTest = NonNullable<"a" | "b" | "c" | null | undefined> // 'a'
+
+
+// Record
+type MyRecord<T extends keyof any, K> = {
+  [P in keyof T]: K
+}
+type MenuKey = 'home' | 'about' | 'more';
+interface Menu {
+  label: string;
+  hidden?: boolean;
+}
+
+type MyRecordTest = MyRecord<MenuKey, Menu> 
+
+// ==================================
+// function
+// ==================================
+
+// ConstructorParameters
+type MyConstructorParameters<T extends new (...args: any) => any> = T extends new (...args: infer P) => any ? P : never
+class Person {
+  constructor(name: string, age?: number) {}
+}
+type MyConstructorParametersTest = MyConstructorParameters<typeof Person>; // [name: string, age?: number]
+
+// Parameters
+type MyParameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never
+type T0 = MyParameters<() => void>; // []
+type T1 = MyParameters<(x: number, y?: string) => void>; // [x: number, y?: string]
+
 // returnType
 type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any) => infer P ? P : any
 
@@ -53,6 +112,6 @@ type ComplexObject = {
 
 const fn = (v: boolean) => v ? 1 : 2
 const fn1 = (v: boolean, w: any) => v ? 1 : 2
-type Test3 = MyReturnType<() => string>
+type MyReturnTypeTest = MyReturnType<() => string>
 
 export {}
