baac58b491974eba258298a81502448897e39c56
qetuozm <yinjianihao@qq.com>
2022-06-06 23:13:42 +0800

    feat: quick sort

--- leetcode/other/sort/quick-sort.js
+++ leetcode/other/sort/quick-sort.js
@@ -1,3 +1,49 @@
+function quickSort_v1(arr) {
+  if (arr.length <= 2) {
+    return arr
+  }
+
+  let baseIndex = arr.length >> 1
+  let base = arr.splice(baseIndex, 1)[0]
+  let left = []
+  let right = []
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] < base) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  return [...quickSort_v1(left), base, ...quickSort_v1(right)]
+}
+
+function quickSort_v2 (arr, left = 0, right = arr.length) {
+  if (left >= right) {
+    return
+  }
+  let x = left
+  let y = right
+  let base = arr[left]
+
+  while(x < y) {
+    // 找到右边比base小的值
+    while(x < y && arr[y] >= base) y--
+    // 交换位置
+    if(x < y) arr[x++] = arr[y]
+
+    // 找到左边比base大的值
+    while(x < y && arr[x] < base) x++
+    // 交换位置
+    if (x < y) arr[y--] = arr[x]
+  }
+
+  arr[x] = base
+  quickSort_v2(arr, left, x - 1)
+  quickSort_v2(arr, x + 1, right)
+  return
+}
+
 // 左递归优化
 function partition(D, low, high) {
   let i
